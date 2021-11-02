d33299f83751edcb546b6825e17558334cc4ee31
sujonmadbor8 <sujonmadbor8@gmail.com>
2021-11-02 09:29:02 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,15 @@
+// falsy:
+// fasle
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy
+// '0', ' ', [], true
+let name = true;
+if (name) {
+    console.log('Condition is true');
+} else {
+    console.log('Condition is false');
+}
