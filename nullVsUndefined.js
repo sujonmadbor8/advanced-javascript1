9b5ff9e9bcc838ed54862d6ac75f4628aa34f1dd
sujonmadbor8 <sujonmadbor8@gmail.com>
2021-11-02 09:40:37 +0600

    s

---
+++ nullVsUndefined.js
@@ -0,0 +1,17 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+const premik = { name: "smart dude", phone: 01312128038 };
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
